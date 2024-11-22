50ed8dee8c16f561d3dca41077492ba392f35139
robwizzie <robertwiscount@gmail.com>
2024-11-22 09:36:26 -0500

    Home Error Fix

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Button } from '../components/ui/button';
 import { useSpotifyAuth } from '../contexts/spotify-auth-context';
 import { useNavigate } from 'react-router-dom';
