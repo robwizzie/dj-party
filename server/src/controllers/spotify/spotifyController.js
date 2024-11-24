df7c5dbeb314f24fbbac631432f49df130b44f9b
David Simmerman <davidsimmermancs@gmail.com>
2024-11-24 00:56:05 -0500

    save auth in cookies and use refresh token

--- server/src/controllers/spotify/spotifyController.js
+++ server/src/controllers/spotify/spotifyController.js
@@ -5,18 +5,24 @@ const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 
 export async function auth(req, res) {
 	try {
-		const { code } = req.body;
+		const { code, refresh_token } = req.body;
 
 		console.log('Attempting to exchange code for token...');
 		console.log('Using redirect URI:', REDIRECT_URI);
 
 		const query = new URLSearchParams({
-			grant_type: 'authorization_code',
-			code: code,
-			redirect_uri: REDIRECT_URI,
 			client_id: CLIENT_ID,
 			client_secret: CLIENT_SECRET
 		});
+
+		if (refresh_token) {
+			query.set('grant_type', 'refresh_token');
+			query.set('refresh_token', refresh_token);
+		} else if (code) {
+			query.set('grant_type', 'authorization_code');
+			query.set('code', code);
+			query.set('redirect_uri', REDIRECT_URI);
+		}
 
 		const response = await fetch(`${TOKEN_URL}?${query.toString()}`, {
 			method: 'POST',
