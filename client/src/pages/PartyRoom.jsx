545e1fa09b5579eea6492acd4151cc66f15e6d1b
David Simmerman <davidsimmermancs@gmail.com>
2024-11-29 17:56:46 -0500

    fix room naming error

--- client/src/pages/PartyRoom.jsx
+++ client/src/pages/PartyRoom.jsx
@@ -30,7 +30,7 @@ function PartyRoomContent() {
 
 	return (
 		<div className="space-y-6">
-			<RoomHeader partyId={partyId} host={user?.display_name} />
+			<PartyHeader partyId={partyId} host={user?.display_name} />
 			<div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
 				<div className="lg:col-span-2 space-y-6">
 					<Player />
